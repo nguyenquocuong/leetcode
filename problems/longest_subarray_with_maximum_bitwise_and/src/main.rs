4e2aab12659ee9bd59c3005e6aa0ecad0cf017ea
Cuong Nguyen <nguyenquocuong.it@gmail.com>
2024-09-14 13:32:14 +0700

    problems: longest_subarray_with_maximum_bitwise_and

--- problems/longest_subarray_with_maximum_bitwise_and/src/main.rs
+++ problems/longest_subarray_with_maximum_bitwise_and/src/main.rs
@@ -2,7 +2,24 @@ struct Solution;
 
 impl Solution {
     pub fn longest_subarray(nums: Vec<i32>) -> i32 {
-        0
+        let mut max_val = nums[0];
+        let mut max_len = 1;
+        let mut current_len = 1;
+
+        for &num in nums.iter().skip(1) {
+            if num > max_val {
+                max_val = num;
+                max_len = 1;
+                current_len = 1;
+            } else if num == max_val {
+                current_len += 1;
+                max_len = max_len.max(current_len);
+            } else {
+                current_len = 0;
+            }
+        }
+
+        max_len
     }
 }
 
